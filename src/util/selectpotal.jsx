20d090df4fa7b60ab928ce145660eb5bd4e067be
dragonrain5761 <dragonrain5761@gmail.com>
2023-07-12 13:20:55 +0900

    셀렉트 수정(useRef 사용)

--- src/util/selectpotal.jsx
+++ src/util/selectpotal.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
+import { StyleSheetManager } from "styled-components";
 
 function Selectpotal() {
   const [showoptions, setShowOptions] = useState(false);
+  const [position, setPosition] = useState({});
   const [values, setValues] = useState("리액트");
   const optionList = ["리액트", "자바", "스프링", "리액트네이티브"];
+  const ref = useRef(null);
+  useEffect(() => {
+    const positions = ref.current.getBoundingClientRect();
+    setPosition({
+      top: positions.top + 42,
+      left: positions.left + 8.6,
+    });
+  }, [ref]);
 
   //누르면 보여주게
   const onclickOptionsHandler = () => {
@@ -20,21 +30,28 @@ function Selectpotal() {
   };
 
   return (
-    <StSelectbutton onClick={onclickOptionsHandler}>
-      <label>{values}</label>
+    //styled-components에서 알 수 없는 속성(left와 top)이 DOM으로 전달되었을 때
+    //발생하는 경고입니다. styled-components는 자동으로 알려지지 않은 속성을
+    //필터링하여 DOM으로 전달하지 않는 기능을 가지고 있습니다.
+    <StyleSheetManager
+      shouldForwardProp={(prop) => prop !== "left" && prop !== "top"}
+    >
+      <StSelectbutton ref={ref} onClick={onclickOptionsHandler}>
+        <label>{values}</label>
 
-      {showoptions &&
-        createPortal(
-          <StoptionContainer>
-            {optionList.map((item) => (
-              <StoptionList key={item} onClick={onChangeOptionHandler}>
-                {item}
-              </StoptionList>
-            ))}
-          </StoptionContainer>,
-          document.body
-        )}
-    </StSelectbutton>
+        {showoptions &&
+          createPortal(
+            <StoptionContainer left={position.left} top={position.top}>
+              {optionList.map((item) => (
+                <StoptionList key={item} onClick={onChangeOptionHandler}>
+                  {item}
+                </StoptionList>
+              ))}
+            </StoptionContainer>,
+            document.body
+          )}
+      </StSelectbutton>
+    </StyleSheetManager>
   );
 }
 
@@ -47,8 +64,8 @@ const StoptionContainer = styled.div`
   border-radius: 12px;
   background-color: #ffffff;
   text-align: center;
-  margin-left: 13.9rem;
-  margin-top: -6.35rem;
+  left: ${(props) => props.left}px;
+  top: ${(props) => props.top}px;
 `;
 
 const StoptionList = styled.div`
